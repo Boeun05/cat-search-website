f70d78ea0a060ef8485ae5544cdd3a9a3ff5f0a6
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2021-03-03 02:02:26 +0900

    async/await

--- src/api.js
+++ src/api.js
@@ -1,13 +1,20 @@
 const API_ENDPOINT =
   "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("api를 가져오는데 문제가 발생하였습니다.");
+    }
+    return response.json();
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
 export const api = {
-  fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
-    );
-  },
-  fetchInfo: (id) => {
-    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => res.json());
-  },
+  fetchCats: (keyword) =>
+    request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`),
+  fetchInfo: (id) => request(`${API_ENDPOINT}/api/cats/${id}`),
 };
